e737a69720d9c36ca038d2684248a58b177930d6
yalonglee <784134748@qq.com>
2019-04-02 17:04:36 +0800

    修改配置文件

--- dist/scripts/env.js
+++ dist/scripts/env.js
@@ -1,12 +1,12 @@
 window.ENV = {
-  restServerUri: '',
-  prometheusUri: '/prometheus',
-  yarnWebPortalUri: '',
-  grafanaUri: '',
-  k8sDashboardUri: '',
-  k8sApiServerUri: '',
-  exporterPort: '',
-  promScrapeTime: ''
+  restServerUri: 'http://10.20.37.175:9186',
+  prometheusUri: 'http://10.20.37.175:9091/prometheus',
+  yarnWebPortalUri: 'http://10.20.37.175:8088',
+  grafanaUri: 'http://10.20.37.175:3000',
+  k8sDashboardUri: 'http://10.20.37.175:9090',
+  k8sApiServerUri: 'http://10.20.37.175:8080',
+  exporterPort: '9100',
+  promScrapeTime: '300s'
 };
 
-window.PAI_PLUGINS = [][0] || [];
+window.PAI_PLUGINS = [[{"uri": "/scripts/plugins/marketplace.bundle.js", "title": "Marketplace"}]][0] || [];
--- env.sh
+++ env.sh
@@ -1,10 +1,18 @@
 #将环境变量写入系统中
-REST_SERVER_URI=http://10.20.37.175:9186
-PROMETHEUS_URI=http://10.20.37.175:9091
-YARN_WEB_PORTAL_URI=http://10.20.37.175:8088
-GRAFANA_URI=http://10.20.37.175:3000
-K8S_DASHBOARD_URI=http://10.20.37.175:9090
-K8S_API_SERVER_URI=http://10.20.37.175:8080
-EXPORTER_PORT=9100
-PROM_SCRAPE_TIME=300s
-WEBPORTAL_PLUGINS=[{"uri":"/scripts/plugins/marketplace.bundle.js","title":"Marketplace"}]
+export REST_SERVER_URI=http://10.20.37.175:9186
+export PROMETHEUS_URI=http://10.20.37.175:9091
+export YARN_WEB_PORTAL_URI=http://10.20.37.175:8088
+export GRAFANA_URI=http://10.20.37.175:3000
+export K8S_DASHBOARD_URI=http://10.20.37.175:9090
+export K8S_API_SERVER_URI=http://10.20.37.175:8080
+export EXPORTER_PORT=9100
+export PROM_SCRAPE_TIME=300s
+
+#切换node版本
+nvm use v8.9.0
+#编译
+npm run build
+#启动
+npm start
+
+
